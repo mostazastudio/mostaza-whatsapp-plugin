6ba0febf881916f2e98ba04456c97dbdb1e18207
Mateo Peralta <mperalta@mostaza.com.co>
2021-11-22 19:01:39 -0500

    cifrados

--- src/components/WhatsaappForm.js
+++ src/components/WhatsaappForm.js
@@ -66,7 +66,7 @@ const WhatsappForm = () => {
                 <input type="text" id="number_field" onChange={(e) => setCelular(e.target.value)} />
                 <label htmlFor="select_field">Quieres ayuda de un asesor para:</label>
                 <select id="select_field" onChange={(e) => setMotivo(e.target.value)}>
-                    {opcionesSelector.map(e=><option>{e}</option>)}
+                    <options>{opcionesSelector}</options>
                 </select>
                 <button>Contactar un asesor</button>
             </div>
