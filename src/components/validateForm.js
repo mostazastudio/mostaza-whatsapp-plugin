09bb880c14540479bec0f14bdcc12d9f8b29cabd
Mateo Peralta <mperalta@mostaza.com.co>
2021-11-28 10:33:58 -0500

    styling

--- src/components/validateForm.js
+++ src/components/validateForm.js
@@ -4,7 +4,7 @@ export default function validateForm(values){
     if (values.nombre.length < 2){
         errors.nombre = "Por favor ingresa tu nombre"
     }
-    if (values.celular.length < 10){
+    if (values.celular.length != 10){
         errors.celular = "Por favor ingresa un celular valido"
     } else if (isNaN(values.celular)) {
         errors.celular = "Por favor ingresa un celular valido"
