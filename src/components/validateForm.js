c08ecda01b71a0cb609d3745a30485a07f18d797
Mateo Peralta <mperalta@mostaza.com.co>
2021-11-29 11:26:28 -0500

    styling

--- src/components/validateForm.js
+++ src/components/validateForm.js
@@ -1,18 +1,21 @@
-export default function validateForm(objeto){
+export default function validateForm(name,value){
     let errors = {}
-
-    if (objeto.nombre?.length < 2){
-        errors.nombre = "Por favor ingresa tu nombre"
+    if (name === "nombre"){
+        if (value.length < 2){
+            errors.nombre = "Por favor ingresa tu nombre"
+        }
     }
-    if (objeto.celular.length != 10){
-        errors.celular = "Por favor ingresa un celular valido"
-    } else if (isNaN(values.celular)) {
-        errors.celular = "Por favor ingresa un celular valido"
+    if (name === "celular"){
+        if (value.length != 10){
+            errors.celular = "Por favor ingresa un celular valido"
+        } else if (isNaN(value)) {
+            errors.celular = "Por favor ingresa un celular valido"
+        }
     }
-
-    if (!objeto.motivo?.length<1){
-        errors.motivo = "por favor selecciona tipo de ayuda"
+    if (name === "motivo"){
+        if (value.length < 2){
+            errors.motivo = "por favor selecciona tipo de ayuda"
+        }
     }
-
     return errors
 }
